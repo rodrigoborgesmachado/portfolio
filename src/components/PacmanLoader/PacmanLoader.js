23b2ac2885a55a0c000df94e523faabd01743c15
Rodrigo Machado <rodrigo.m@condocontrol.com>
2025-09-15 09:54:06 -0300

    Colocando loading centralizado

--- src/components/PacmanLoader/PacmanLoader.js
+++ src/components/PacmanLoader/PacmanLoader.js
@@ -8,19 +8,21 @@ export default function PacmanLoader({
 }) {
   const pellets = Array.from({ length: dots }, (_, i) => i);
   return (
-    <div
-      className="pacman-loader"
-      role="status"
-      aria-label="Carregando…"
-      style={{
-        '--size': `${size}px`,
-        '--mouth': `${mouthAngle}deg`,
-        '--dots': dots
-      }}
-    >
-      <div className="pacman" />
-      <div className="pellets" aria-hidden="true">
-        {pellets.map(i => <span key={i} className="pellet" style={{ '--i': i }} />)}
+    <div className='loader-container'>
+      <div
+        className="pacman-loader"
+        role="status"
+        aria-label="Carregando…"
+        style={{
+          '--size': `${size}px`,
+          '--mouth': `${mouthAngle}deg`,
+          '--dots': dots
+        }}
+      >
+        <div className="pacman" />
+        <div className="pellets" aria-hidden="true">
+          {pellets.map(i => <span key={i} className="pellet" style={{ '--i': i }} />)}
+        </div>
       </div>
     </div>
   );
