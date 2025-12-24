de60f25756231d26f6605f74d575f59bd7278807
Rodrigo Machado <rodrigo.m@condocontrol.com>
2025-12-24 12:30:34 -0300

    Adicionando imagem no portfolio

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,15 +3,27 @@ import ProjetosPagged from '../../components/ProjetosPagged/projetosPagged';
 import RepositoryPagged from '../../components/RepositoryPagged/repositoryPagged';
 
 export default function Home(){
+    const profileImages = [
+        'https://www.sunsalesystem.com.br/img/rodrigo-1.jpeg',
+        'https://www.sunsalesystem.com.br/img/rodrigo-2.jpeg',
+        'https://www.sunsalesystem.com.br/img/rodrigo-3.jpeg',
+    ];
+    const profileImage = profileImages[Math.floor(Math.random() * profileImages.length)];
+
     return(
         <div className="container">
             <div className="presentation">
-                <h1>
-                    Hello,
-                </h1>
-                <h3>
-                    Seja bem vindo ao habitat natural de Rodrigo Machado
-                </h3>
+                <div className="presentation-avatar">
+                    <img src={profileImage} alt="Rodrigo Machado" />
+                </div>
+                <div className='presentation-message'>
+                    <h1>
+                        Hello,
+                    </h1>
+                    <h3>
+                        Seja bem vindo ao habitat natural de Rodrigo Machado
+                    </h3>
+                </div>
             </div>
             <div className='projects'>
                 <ProjetosPagged tipo={0}/>
